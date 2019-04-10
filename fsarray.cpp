c4492a0e3271cd162dc6ec99b16b2137c779cafe
Carlos R. Arias <arias@spu.edu>
2019-04-10 12:16:14 -0700

    Insert finished, we need to test it though

--- fsarray.cpp
+++ fsarray.cpp
@@ -15,15 +15,18 @@ FSArray::FSArray(size_t capacity) : _capacity(capacity){
 	}
 }
 FSArray::FSArray(const FSArray& list) : _capacity(list._capacity){
-	// Is there a bug here?
+	// Is there a bug here? --> Solved by setting _size to be protected
 	_size = list._size;
 	_data = new Object*[_capacity];
-	for (size_t i = 0; i < _capacity; i++) {
+	for (size_t i = 0; i < _size; i++) { // Issa correction to the _capacity
 		_data[i] = list.Get(i)->Clone();
+	}
+	for (size_t i = _size; i < _capacity; i++){
+		_data[i] = nullptr;
 	}
 }
 FSArray::~FSArray(){
-	for (size_t i = 0; i < Size(); i++) {
+	for (size_t i = 0; i < _size; i++) {
 		delete _data[i];
 	}
 	delete[] _data;
@@ -48,7 +51,23 @@ const FSArray& FSArray::operator=(const FSArray& rhs){
 }
 
 bool FSArray::Insert(Object* element, size_t position){
-	return false;
+	// Check if the position is valid
+	if (position > _size)
+		return false;
+	// Check if there is enough space
+	if (_capacity == _size)
+		return false;
+
+	if (position < _size){ // We need to shift
+		// WARNING WILL ROBINSON : Lost in Space Reference
+		// Be careful with size_t when subtracting
+		for (size_t i = _size; i > position; i--){
+			_data[i] = _data[i - 1]; 
+		}
+	}
+	_data[position] = element;
+	_size++;
+	return true;
 }
 int FSArray::IndexOf(Object* element)const{
 	return -1;
