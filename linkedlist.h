2d43903d0b5405b4df2ab3bbd60f4d66ce7c184c
Carlos R. Arias <arias@spu.edu>
2019-04-15 09:59:41 -0700

    Linked List header file

---
+++ linkedlist.h
@@ -0,0 +1,26 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+
+class LinkedList : public Object{
+		struct Node{
+			Object* data;
+			Node* next;
+		};
+		Node* _head;
+		LinkedList(const LinkedList&);
+		const LinkedList& operator=(const LinkedList&);
+	public:
+		LinkedList();
+		virtual ~LinkedList();
+
+		virtual bool Insert(Object* element, size_t position);
+		virtual int IndexOf(Object* element)const;
+		virtual Object* Remove(size_t position);
+		virtual Object* Get(size_t position)const;
+		virtual string ToString()const;
+		virtual void Clear();
+};
+
+
+#endif /* end of include guard: LINKEDLIST_H */
